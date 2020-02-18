c7ef6d57634eb1862c6f972e24c23fb71ed28d7f
Gabriel Town <rieltown@gmail.com>
2020-02-18 14:04:44 -0700

    added different images for layer1Click

--- public_html/scripts/intro.js
+++ public_html/scripts/intro.js
@@ -62,35 +62,35 @@ function layer1Click(){
 	console.log(layer1);
 	if (layer1 === 'on') {
 		//change layer1 sidebar image to on
-
+		document.getElementById("in-sb-l1").src="pics/intro/sb/sb-in-l1.jpg";
 		if (layer0 === 'on' && layer2 === 'on') {
 			//set image to original
-
+			document.getElementById("introImage").src="pics/intro/layer-basics.jpg";
 		} else if (layer0 === 'on' && layer2 === 'off') {
-			//set image to faceGone
-
+			//set image to face gone
+			document.getElementById("introImage").src="pics/intro/layer-basics-face-gone.jpg";
 		} else if (layer0 === 'off' && layer2 === 'on') {
-			//set image to blueGone
-
+			//set image to blue gone
+			document.getElementById("introImage").src="pics/intro/layer-basics-blue-gone.jpg";
 		} else {
-			//set image greenOnly
-
+			//set image only green
+			document.getElementById("introImage").src="pics/intro/layer-basics-only-green.jpg";
 		}
 	} else if (layer1 === 'off') {
 		//change layer1 sidebar image to off
-
+		document.getElementById("in-sb-l1").src="pics/intro/sb/sb-in-l1-off.jpg";
 		if (layer0 === 'on' && layer2 === 'on') {
-			//set image to greenGone
-
+			//set image to green gone
+			document.getElementById("introImage").src="pics/intro/layer-basics-green-gone.jpg";
 		} else if (layer0 === 'on' && layer2 === 'off') {
-			//set image to blueOnly
-
+			//set image to only blue
+			document.getElementById("introImage").src="pics/intro/layer-basics-only-blue.jpg";
 		} else if (layer0 === 'off' && layer2 === 'on') {
-			//set image to greenOnly
-
+			//set image to only green
+			document.getElementById("introImage").src="pics/intro/layer-basics-only-green.jpg";
 		} else {
 			//set image to allGone
-
+			document.getElementById("introImage").src="pics/intro/layer-basics-all-gone.jpg";
 		}
 	}
 } // end of layer1Click
@@ -102,7 +102,7 @@ function layer2Click() {
 	console.log(layer2);
 	if (layer2 === 'on') {
 		//change layer2 sidebar image to on
-
+		document.getElementById("in-sb-l2").src="pics/intro/sb/sb-in-l2.jpg";
 		if (layer1 === 'on' && layer0 === 'on') {
 			//set image to original
 		} else if (layer1 === 'on' && layer0 === 'off') {
@@ -114,7 +114,7 @@ function layer2Click() {
 		}
 	} else if (layer2 === 'off') {
 		//change layer2 sidebar image to off
-
+		document.getElementById("in-sb-l2").src="pics/intro/sb/sb-in-l2-0ff.jpg";
 		if (layer1 === 'on' && layer0 === 'on') {
 			//set image to faceGone
 
