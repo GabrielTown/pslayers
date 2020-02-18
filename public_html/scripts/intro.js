75f1619cc95a32f1184c2cd9d183509c4c306307
Gabriel Town <rieltown@gmail.com>
2020-02-18 14:38:03 -0700

    deleted console log and commented out function, added opacity.js file

--- public_html/scripts/intro.js
+++ public_html/scripts/intro.js
@@ -15,10 +15,8 @@ function layerToggle(layer) {
 }
 
 function layer0Click() {
-	console.log("layer0 was clicked");
 	//toggle layer 0
 	layer0 = layerToggle(layer0);
-	console.log(layer0);
 	if (layer0 === 'on') {
 		//change layer0 sidebar image to on
 		document.getElementById("in-sb-l0").src="pics/intro/sb/sb-in-l0.jpg";
@@ -56,10 +54,8 @@ function layer0Click() {
 
 
 function layer1Click(){
-	console.log("layer1 was clicked");
 	//toggle layer 1
 	layer1 = layerToggle(layer1);
-	console.log(layer1);
 	if (layer1 === 'on') {
 		//change layer1 sidebar image to on
 		document.getElementById("in-sb-l1").src="pics/intro/sb/sb-in-l1.jpg";
@@ -97,9 +93,7 @@ function layer1Click(){
 
 
 function layer2Click() {
-	console.log("layer2 was clicked");
 	layer2 = layerToggle(layer2);
-	console.log(layer2);
 	if (layer2 === 'on') {
 		//change layer2 sidebar image to on
 		document.getElementById("in-sb-l2").src="pics/intro/sb/sb-in-l2.jpg";
@@ -118,7 +112,7 @@ function layer2Click() {
 		}
 	} else if (layer2 === 'off') {
 		//change layer2 sidebar image to off
-		document.getElementById("in-sb-l2").src="pics/intro/sb/sb-in-l2-off.jpg";
+		document.	getElementById("in-sb-l2").src="pics/intro/sb/sb-in-l2-off.jpg";
 		if (layer1 === 'on' && layer0 === 'on') {
 			//set image to face gone
 			document.getElementById("introImage").src="pics/intro/layer-basics-face-gone.jpg";
@@ -134,123 +128,3 @@ function layer2Click() {
 		}
 	}
 } //end of layer2Click
-
-
-/*
-function setDisplayImage() {
-	//check to see if layer0 was clicked
-	if (introLayer0.addEventListener('click', onClick())) { //TODO figure out the logic for when a layer is clicked return true
-		console.log("layer0 was clicked");
-		//toggle layer 0
-		layer0 = layerToggle(layer0);
-		if (layer0 === 'on') {
-			//change layer0 sidebar image to on
-
-			if (layer1 === 'on' && layer2 === 'on') {
-				//set image to original
-
-			} else if (layer1 === 'on' && layer2 === 'off') {
-				//set image to faceGone
-
-			} else if (layer1 === 'off' && layer2 === 'on') {
-				//set image to greenGone
-
-			} else {
-				//set image blueOnly
-
-			}
-		} else if (layer0 === 'off') {
-			//change layer0 sidebar image to off
-
-			if (layer1 === 'on' && layer2 === 'on') {
-				//set image to blueGone
-
-			} else if (layer1 === 'on' && layer2 === 'off') {
-				//set image to greenOnly
-
-			} else if (layer1 === 'off' && layer2 === 'on') {
-				//set image to faceOnly
-
-			} else {
-				//set image to allGone
-
-			}
-		}
-	} //end of layer0 click
-
-	//check to see if layer1 was clicked
-	else if (introLayer1.addEventListener('click') === true) {
-		console.log("layer1 was clicked");
-		//toggle layer 1
-		layer1 = layerToggle(layer1);
-		if (layer1 === 'on') {
-			//change layer1 sidebar image to on
-
-			if (layer0 === 'on' && layer2 === 'on') {
-				//set image to original
-
-			} else if (layer0 === 'on' && layer2 === 'off') {
-				//set image to faceGone
-
-			} else if (layer0 === 'off' && layer2 === 'on') {
-				//set image to blueGone
-
-			} else {
-				//set image greenOnly
-
-			}
-		} else if (layer1 === 'off') {
-			//change layer1 sidebar image to off
-
-			if (layer0 === 'on' && layer2 === 'on') {
-				//set image to greenGone
-
-			} else if (layer0 === 'on' && layer2 === 'off') {
-				//set image to blueOnly
-
-			} else if (layer0 === 'off' && layer2 === 'on') {
-				//set image to greenOnly
-
-			} else {
-				//set image to allGone
-
-			}
-		}
-	} //end of layer1 click
-
-	//layer2 was clicked
-	else {
-		console.log("layer2 was clicked");
-		layer2 = layerToggle(layer2);
-		if (layer2 === 'on') {
-			//change layer2 sidebar image to on
-
-			if (layer1 === 'on' && layer0 === 'on') {
-				//set image to original
-			} else if (layer1 === 'on' && layer0 === 'off') {
-				//set image to blueGone
-			} else if (layer1 === 'off' && layer0 === 'on') {
-				//set image to greenGone
-			} else {
-				//set image faceOnly
-			}
-		} else if (layer2 === 'off') {
-			//change layer2 sidebar image to off
-
-			if (layer1 === 'on' && layer0 === 'on') {
-				//set image to faceGone
-
-			} else if (layer1 === 'on' && layer0 === 'off') {
-				//set image to greenOnly
-
-			} else if (layer1 === 'off' && layer0 === 'on') {
-				//set image to blueOnly
-
-			} else {
-				//set image to allGone
-
-			}
-		}
-	} //end of layer2 click
-} // end of setDisplayImage
-*/
