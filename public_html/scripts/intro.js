6d3bde7f9fdc213b0ca5e330bfc12e1499334138
Gabriel Town <rieltown@gmail.com>
2020-02-18 09:08:02 -0700

    cleaned up comments

--- public_html/scripts/intro.js
+++ public_html/scripts/intro.js
@@ -1,5 +1,6 @@
-let  introLayer1 = document.getElementById('in-sb-l1');
-let  introLayer0 = document.getElementById('in-sb-l0');
+let introLayer2 = document.getElementById('in-sb-l2');
+let introLayer1 = document.getElementById('in-sb-l1');
+let introLayer0 = document.getElementById('in-sb-l0');
 
 function layerToggle(layer) {
 	if (layer === 'on') {
@@ -9,9 +10,14 @@ function layerToggle(layer) {
 	}
 }
 
+function onClick () {
+	return 'true';
+}
+
+
 function setDisplayImage() {
 	//check to see if layer0 was clicked
-	if ((introLayer0.addEventListener('click')) === true) {
+	if (introLayer0.addEventListener('click', onClick())) { //TODO figure out the logic for when a layer is clicked return true
 		console.log("layer0 was clicked");
 		//toggle layer 0
 		layer0 = layerToggle(layer0);
@@ -20,24 +26,32 @@ function setDisplayImage() {
 
 			if (layer1 === 'on' && layer2 === 'on') {
 				//set image to original
+
 			} else if (layer1 === 'on' && layer2 === 'off') {
 				//set image to faceGone
+
 			} else if (layer1 === 'off' && layer2 === 'on') {
 				//set image to greenGone
+
 			} else {
 				//set image blueOnly
+
 			}
 		} else if (layer0 === 'off') {
 			//change layer0 sidebar image to off
 
 			if (layer1 === 'on' && layer2 === 'on') {
 				//set image to blueGone
+
 			} else if (layer1 === 'on' && layer2 === 'off') {
 				//set image to greenOnly
+
 			} else if (layer1 === 'off' && layer2 === 'on') {
 				//set image to faceOnly
+
 			} else {
 				//set image to allGone
+
 			}
 		}
 	} //end of layer0 click
@@ -48,28 +62,36 @@ function setDisplayImage() {
 		//toggle layer 1
 		layer1 = layerToggle(layer1);
 		if (layer1 === 'on') {
-			//change layer0 sidebar image to on
+			//change layer1 sidebar image to on
 
 			if (layer0 === 'on' && layer2 === 'on') {
 				//set image to original
+
 			} else if (layer0 === 'on' && layer2 === 'off') {
 				//set image to faceGone
+
 			} else if (layer0 === 'off' && layer2 === 'on') {
 				//set image to blueGone
+
 			} else {
 				//set image greenOnly
+
 			}
 		} else if (layer1 === 'off') {
 			//change layer1 sidebar image to off
 
 			if (layer0 === 'on' && layer2 === 'on') {
 				//set image to greenGone
+
 			} else if (layer0 === 'on' && layer2 === 'off') {
-				//set image to bluenly
+				//set image to blueOnly
+
 			} else if (layer0 === 'off' && layer2 === 'on') {
 				//set image to greenOnly
+
 			} else {
 				//set image to allGone
+
 			}
 		}
 	} //end of layer1 click
@@ -79,7 +101,7 @@ function setDisplayImage() {
 		layer2 = layerToggle(layer2);
 		console.log("layer2 was clicked");
 		if (layer2 === 'on') {
-			//change layer0 sidebar image to on
+			//change layer2 sidebar image to on
 
 			if (layer1 === 'on' && layer0 === 'on') {
 				//set image to original
@@ -95,12 +117,16 @@ function setDisplayImage() {
 
 			if (layer1 === 'on' && layer0 === 'on') {
 				//set image to faceGone
+
 			} else if (layer1 === 'on' && layer0 === 'off') {
 				//set image to greenOnly
+
 			} else if (layer1 === 'off' && layer0 === 'on') {
 				//set image to blueOnly
+
 			} else {
 				//set image to allGone
+
 			}
 		}
 	} //emd of layer2 click
