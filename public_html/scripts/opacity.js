ccbb593f56a8313289b1c89cf48439cb7fa982bc
Gabriel Town <rieltown@gmail.com>
2020-02-18 15:00:59 -0700

    deleted extra html code

---
+++ public_html/scripts/opacity.js
@@ -0,0 +1,8 @@
+function showSlider() {
+	<form>
+	<div data-role="rangeslider">
+		<label for="range-1b">Rangeslider:</label>
+	<input type="range" name="range-1b" id="range-1b" min="0" max="100" value="80">
+		</div>
+		</form>
+}
