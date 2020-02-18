ba990b69fa74c6a13b67ba8599ddbe9745c1501c
Gabriel Town <rieltown@gmail.com>
2020-02-18 13:56:51 -0700

    added different images for layer0Click

--- public_html/scripts/intro.js
+++ public_html/scripts/intro.js
@@ -14,10 +14,6 @@ function layerToggle(layer) {
 	}
 }
 
-function onClick () {
-	return 'true';
-}
-
 function layer0Click() {
 	console.log("layer0 was clicked");
 	//toggle layer 0
@@ -25,35 +21,35 @@ function layer0Click() {
 	console.log(layer0);
 	if (layer0 === 'on') {
 		//change layer0 sidebar image to on
-
+		document.getElementById("in-sb-l0").src="pics/intro/sb/sb-in-l0.jpg";
 		if (layer1 === 'on' && layer2 === 'on') {
 			//set image to original
-
+			document.getElementById("introImage").src="pics/intro/layer-basics.jpg";
 		} else if (layer1 === 'on' && layer2 === 'off') {
-			//set image to faceGone
-
+			//set image to face gone
+			document.getElementById("introImage").src="pics/intro/layer-basics-face-gone.jpg";
 		} else if (layer1 === 'off' && layer2 === 'on') {
-			//set image to greenGone
-
+			//set image to green gone
+			document.getElementById("introImage").src="pics/intro/layer-basics-green-gone.jpg";
 		} else {
-			//set image blueOnly
-
+			//set image only blue
+			document.getElementById("introImage").src="pics/intro/layer-basics-only-blue.jpg";
 		}
 	} else if (layer0 === 'off') {
 		//change layer0 sidebar image to off
-
+		document.getElementById("in-sb-l0").src="pics/intro/sb/sb-in-l0-off.jpg";
 		if (layer1 === 'on' && layer2 === 'on') {
-			//set image to blueGone
-
+			//set image to blue gone
+			document.getElementById("introImage").src="pics/intro/layer-basics-blue-gone.jpg";
 		} else if (layer1 === 'on' && layer2 === 'off') {
-			//set image to greenOnly
-
+			//set image to only green
+			document.getElementById("introImage").src="pics/intro/layer-basics-only-green.jpg";
 		} else if (layer1 === 'off' && layer2 === 'on') {
-			//set image to faceOnly
-
+			//set image to only face
+			document.getElementById("introImage").src="pics/intro/layer-basics-only-face.jpg";
 		} else {
-			//set image to allGone
-
+			//set image to all gone
+			document.getElementById("introImage").src="pics/intro/layer-basics-all-gone.jpg";
 		}
 	}
 } //end of layer0Click
