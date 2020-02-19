eda25d8ef80640608546fdf3fe892ad93c97e094
Gabriel Town <rieltown@gmail.com>
2020-02-19 10:57:03 -0700

    changed gitignore

--- public_html/scripts/opacity.js
+++ public_html/scripts/opacity.js
@@ -1,9 +1,5 @@
-/*
-function showSlider() {
-	<form>
-	<div data-role="rangeslider">
-		<label for="range-1b">Rangeslider:</label>
-	<input type="range" name="range-1b" id="range-1b" min="0" max="100" value="80">
-		</div>
-		</form>
-}*/
+
+function getOpacity() {
+	let op = 50; //TODO figure how to get slidebar data
+		document.getElementById("opacityCircle").setAttribute("style","opacity:op/100; -moz-opacity:op/100; filter:alpha(opacity=op)");
+}
