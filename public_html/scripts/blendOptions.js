e2d1d425d19a6044f5ed7630fed8861690e68a8c
Gabriel Town <rieltown@gmail.com>
2020-02-18 20:03:14 -0700

    added the rest of the images in blendOptions.js, section works

--- public_html/scripts/blendOptions.js
+++ public_html/scripts/blendOptions.js
@@ -9,21 +9,21 @@ function darkenColorClick() {
 }
 
 function lightenClick(){
-
+	document.getElementById("bo-sb-middle").src="pics/blend-options/sb/sb-bo-middle-lighten.jpg";
 	document.getElementById("blendOptions").src="pics/blend-options/lighten.jpg";
 }
 
 function overlayClick(){
-
+	document.getElementById("bo-sb-middle").src="pics/blend-options/sb/sb-bo-middle-overlay.jpg";
 	document.getElementById("blendOptions").src="pics/blend-options/overlay.jpg";
 }
 
 function vividLightClick(){
-
+	document.getElementById("bo-sb-middle").src="pics/blend-options/sb/sb-bo-middle-vivid.jpg";
 	document.getElementById("blendOptions").src="pics/blend-options/vivid-light.jpg";
 }
 
 function hardMixClick(){
-
+	document.getElementById("bo-sb-middle").src="pics/blend-options/sb/sb-bo-middle-hardmix.jpg";
 	document.getElementById("blendOptions").src="pics/blend-options/hard-mix.jpg";
 }
