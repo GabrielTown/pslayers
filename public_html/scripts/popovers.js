c7a48cbc857b2cb047272054ec9e5f1ccab85375
Gabriel Town <rieltown@gmail.com>
2020-02-24 16:45:35 -0700

    successfully added a popover

---
+++ public_html/scripts/popovers.js
@@ -0,0 +1,7 @@
+$(function () {
+	$('[data-toggle="popover"]').popover({
+		trigger: 'hover | click',
+		html : true,
+		content: '<div class="media"><a href="#" class="pull-right"><img src="pics/intro/side-bar-intro.jpg" width=65%" class="media-object" alt="Sample Sidebar"></a></div>'
+	})
+});
