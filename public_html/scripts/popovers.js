de23437592eb1602d39baff9ed2d1b8811d26d21
Gabriel Town <rieltown@gmail.com>
2020-02-27 09:17:02 -0700

    finished intro paragraphs

--- public_html/scripts/popovers.js
+++ public_html/scripts/popovers.js
@@ -13,7 +13,7 @@ $(function () {
 		placement : 'auto',
 		trigger: 'hover',
 		html : true,
-		content: '<div class="media mx-auto"><a href="#" class="pull-up"><img src="pics/popovers/clippy.jpg" width="100%" class="media-object mx-auto" alt="Cool Car Bro"></a></div>'
+		content: '<div class="media mx-auto"><a href="#" class="pull-up"><img src="pics/popovers/clippy.jpg" width="100%" class="media-object mx-auto" alt="Microsoft Clippy"></a></div>'
 	})
 });
 
@@ -26,3 +26,20 @@ $(function () {
 	})
 });
 
+$(function () {
+	$('[data-toggle="eye"]').popover({
+		placement : 'auto',
+		trigger: 'hover',
+		html : true,
+		content: '<div class="media mx-auto"><a href="#" class="pull-up"><img src="pics/popovers/eyes.jpg" width="100%" class="media-object mx-auto" alt="Sidebar Eyes"></a></div>'
+	})
+});
+
+$(function () {
+	$('[data-toggle="checkered"]').popover({
+		placement : 'auto',
+		trigger: 'hover',
+		html : true,
+		content: '<div class="media mx-auto"><a href="#" class="pull-up"><img src="pics/popovers/checkered.jpg" width="100%" class="media-object mx-auto" alt="Checkered Pattern"></a></div>'
+	})
+});
