264a4b484bb613c226afcccff7d5e691a63a5716
Gabriel Town <rieltown@gmail.com>
2020-02-26 21:56:46 -0700

    started working on intro word content adding popovers where needed

--- public_html/scripts/popovers.js
+++ public_html/scripts/popovers.js
@@ -1,7 +1,28 @@
 $(function () {
-	$('[data-toggle="popover"]').popover({
-		trigger: 'hover | click',
+	$('[data-toggle="sidebar"]').popover({
+		placement : 'auto',
+		trigger: 'hover',
 		html : true,
-		content: '<div class="media"><a href="#" class="pull-right"><img src="pics/intro/side-bar-intro.jpg" width=65%" class="media-object" alt="Sample Sidebar"></a></div>'
+		content: '<div class="media mx-auto"><a href="#" class="pull-right"><img src="pics/intro/side-bar-intro.jpg" width=100%" class="media-object mx-auto" alt="Sample Sidebar"></a></div>'
 	})
 });
+
+
+$(function () {
+	$('[data-toggle="clippy"]').popover({
+		placement : 'auto',
+		trigger: 'hover',
+		html : true,
+		content: '<div class="media mx-auto"><a href="#" class="pull-up"><img src="pics/popovers/clippy.jpg" width="100%" class="media-object mx-auto" alt="Cool Car Bro"></a></div>'
+	})
+});
+
+$(function () {
+	$('[data-toggle="crap"]').popover({
+		placement : 'auto',
+		trigger: 'hover',
+		html : true,
+		content: '<div class="media mx-auto"><a href="#" class="pull-up"><img src="pics/popovers/coolcarbro.png" width="100%" class="media-object mx-auto" alt="Cool Car Bro"></a></div>'
+	})
+});
+
