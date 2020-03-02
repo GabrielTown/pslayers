e61e8111deefb6647f4705ad6702f31e57bce383
Gabriel Town <rieltown@gmail.com>
2020-03-01 18:22:56 -0700

    added opacity paragraphs

--- public_html/scripts/animation.js
+++ public_html/scripts/animation.js
@@ -18,3 +18,19 @@ function addInfiniteFlash(imageId) {
 		image.classList.remove("infiniteFlash");
 	});
 }
+
+function readMore() {
+	const dots = document.getElementById("dots");
+	const moreText = document.getElementById("more");
+	const btnText = document.getElementById("readMore");
+
+	if (dots.style.display === "none") {
+		dots.style.display = "inline";
+		btnText.innerHTML = "Read more";
+		moreText.style.display = "none";
+	} else {
+		dots.style.display = "none";
+		btnText.innerHTML = "Read less";
+		moreText.style.display = "inline";
+	}
+}
--- public_html/scripts/popovers.js
+++ public_html/scripts/popovers.js
@@ -15,7 +15,6 @@ $(function () {
 	})
 });
 
-
 $(function () {
 	$('[data-toggle="eye"]').popover({
 		placement : 'auto',
@@ -31,5 +30,14 @@ $(function () {
 		trigger: 'hover',
 		html : true,
 		content: '<div class="media mx-auto"><a href="#" class="pull-up"><img src="pics/popovers/checkered.jpg" width="100%" class="media-object mx-auto" alt="Checkered Pattern"></a></div>'
+	})
+});
+
+$(function () {
+	$('[data-toggle="opacity"]').popover({
+		placement : 'auto',
+		trigger: 'click',
+		html : true,
+		content: '<div class="media mx-auto"><a href="#" class="pull-up"><img src="pics/popovers/opacitySidebar.jpg" width="100%" class="media-object mx-auto" alt="Sidebar Opacity"></a></div>'
 	})
 });
