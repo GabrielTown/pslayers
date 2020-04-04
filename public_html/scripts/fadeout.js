432dd342162ff2a9d911bd469868dbc54cbeade0
Gabriel Town <rieltown@gmail.com>
2020-04-03 20:44:58 -0600

    disabled fadeOut functions while infiniteFade function is on

--- public_html/scripts/fadeout.js
+++ public_html/scripts/fadeout.js
@@ -1,42 +1,50 @@
+let infiniteFadeOn = false;
+
 function fadeOut(section, doNotFade, id) {
-	//get an array of each element of section
-	const fade = Array.from(document.querySelectorAll(`.${section}`));
-	//get an array of each element to zoom/unfade
-	const noFade = Array.from(document.querySelectorAll(`.${doNotFade}`));
+	if (!infiniteFadeOn) {
+		console.log("here how");
+		//get an array of each element of section
+		const fade = Array.from(document.querySelectorAll(`.${section}`));
+		//get an array of each element to zoom/unfade
+		const noFade = Array.from(document.querySelectorAll(`.${doNotFade}`));
 
-	//add faded class o each element in section
-	fade.forEach(element => element.classList.add('faded'));
-	//delete faded class from elements not faded
-	noFade.forEach(element => element.classList.remove('faded'));
-	//add zoom class to not faded elements
-	noFade.forEach(element => element.classList.add('zoom'));
+		//add faded class o each element in section
+		fade.forEach(element => element.classList.add('faded'));
+		//delete faded class from elements not faded
+		noFade.forEach(element => element.classList.remove('faded'));
+		//add zoom class to not faded elements
+		noFade.forEach(element => element.classList.add('zoom'));
 
-	//once mouse leaves the keyword remove all classes just added
-	document.getElementById(id).addEventListener("mouseout", removeFade, false);
+		//once mouse leaves the keyword remove all classes just added
+		document.getElementById(id).addEventListener("mouseout", removeFade, false);
 
-	//remove fade and zoom
-	function removeFade() {
-		fade.forEach(element => element.classList.remove('faded', 'zoom'));
+		//remove fade and zoom
+		function removeFade() {
+			fade.forEach(element => element.classList.remove('faded', 'zoom'));
+		}
 	}
+	console.log(infiniteFadeOn);
 }
 
 //basically the same as fadeout function, but want to show the text and what to click until the user clicks it
 function infiniteFade(section, doNotFade, doNotFade2) {
+	infiniteFadeOn = true;
 	const fade = Array.from(document.querySelectorAll(`.${section}`));
 	const noFade = Array.from(document.querySelectorAll(`.${doNotFade}`));
-	const noFade2 = document.querySelector(`.${doNotFade2}`);
+	const sentence = document.querySelector(`.${doNotFade2}`);
 
 	fade.forEach(element => element.classList.add('faded'));
 	noFade.forEach(element => element.classList.remove('faded'));
 	noFade.forEach(element => element.classList.add('noZoom'));
 
-	noFade2.classList.remove('faded');
+	sentence.classList.remove('faded');
 
 	noFade.forEach(div => div.addEventListener("click", removeFade, false));
 
 	//remove fade and zoom
 	function removeFade() {
 		fade.forEach(element => element.classList.remove('faded', 'noZoom'));
+		infiniteFadeOn = false;
 	}
 }
 
